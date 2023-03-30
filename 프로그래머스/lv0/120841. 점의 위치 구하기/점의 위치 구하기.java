4f86a7ffe909a11eb741b6072de071d5e91b04d7
林지현 <103114387+mic050r@users.noreply.github.com>
2023-03-31 08:34:21 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] dot) {
+        int answer = 0;
+        int x = dot[0];
+        int y = dot[1];
+        if(x > 0){ 
+            if(y > 0) answer = 1;
+            else answer = 4;
+        }else{ // x < 0
+            if(y > 0) answer = 2;
+            else  answer = 3;
+        }
+        
+        return answer;
+    }
+}
