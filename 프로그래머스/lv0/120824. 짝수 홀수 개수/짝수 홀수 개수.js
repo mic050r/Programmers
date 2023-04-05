9b8d4230f1755b516811a1eed366acda83390cc2
林지현 <103114387+mic050r@users.noreply.github.com>
2023-04-05 11:36:53 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -2,11 +2,7 @@ function solution(num_list) {
     var answer = [];
     var odd = 0, even = 0; //홀수 , 짝수
     for(var i = 0; i < num_list.length; i++){
-        if(num_list[i] % 2 == 0){
-            even++;
-        }else{
-            odd++;
-        }
+        var answer = (num_list[i] % 2 == 0) ? even++ : odd++;
     }
     return [even,odd];
 }
