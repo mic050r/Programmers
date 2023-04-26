b3e15e8e279573f77ae88a811f071ba44d32d03a
林지현 <103114387+mic050r@users.noreply.github.com>
2023-04-26 13:30:36 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,10 +1,10 @@
 function solution(a, b) {
     var answer = 0;
-    if(a%2!=0 && b%2!=0){ // 모두 홀수 라면
+    if(a%2!=0 && b%2!=0){ // a와 b가 모두 홀수라면 
         answer = a*a+b*b;
-    }else if(a%2!=0 || b%2!=0){
+    }else if(a%2!=0 || b%2!=0){ // a와 b 중 하나만 홀수라면
         answer = 2*(a+b);
-    }else{
+    }else{ // a와 b 모두 홀수가 아니라면
         answer = Math.abs(a-b);
     }
     return answer;
