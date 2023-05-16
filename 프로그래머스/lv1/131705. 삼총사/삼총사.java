824a6319af1b72e0f0fe0e35f6bcf06778beb235
林지현 <103114387+mic050r@users.noreply.github.com>
2023-05-16 21:40:54 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        int cnt = 0;
+        for(int i = 0; i < number.length; i++){
+            for(int j = i+1; j < number.length; j++){
+                for(int k = j+1; k < number.length; k++){
+                    answer = number[i] + number[j] + number[k];
+                    if(answer == 0) cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
