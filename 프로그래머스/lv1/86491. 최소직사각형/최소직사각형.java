10a61375363354b1f193e91f12aea14afcd75ce1
林지현 <103114387+mic050r@users.noreply.github.com>
2023-05-16 23:34:21 +0900

    [level 1] Title: 최소직사각형, Time: 2.07 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int temp = 0;
+        int maxW = 0;
+        int maxH = 0;
+        int max = 10001;
+        // 더 큰 값을 가로에 넣기
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][0] < sizes[i][1]){
+                temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+        }
+        for(int i = 0; i < sizes.length; i++){
+            maxW = Math.max(maxW, sizes[i][0]);
+            maxH = Math.max(maxH, sizes[i][1]);
+        }
+        answer = maxW*maxH;
+        return answer;
+    }
+}
