89f0a4908fae45a9ac617370588585ec842c613c
林지현 <103114387+mic050r@users.noreply.github.com>
2023-05-30 09:48:36 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,9 @@
+function solution(dot) {
+    var answer = 0;
+    let x = dot[0], y = dot[1];
+    if(x > 0 && y > 0) answer = 1;
+    if(x < 0 && y > 0) answer = 2;
+    if(x < 0 && y < 0) answer = 3;
+    if(x > 0 && y < 0) answer = 4;
+    return answer;
+}
