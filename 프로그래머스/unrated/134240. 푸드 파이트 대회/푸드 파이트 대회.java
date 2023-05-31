f42593f21405a2490c18f11e483faade56f73a06
林지현 <103114387+mic050r@users.noreply.github.com>
2023-05-31 17:11:19 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 10.80 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        int index = 1;
+        for(int i = 1; i < food.length; i++){
+            if(food[i] >= 2 ){
+                for(int k = 0; k < food[i]/2; k++){
+                    answer += i;
+                }
+            }
+        }
+        answer += 0;
+        for(int i = food.length -1; i > 0; i--){
+            if(food[i] >= 2 ){
+                for(int k = 0; k < food[i]/2; k++){
+                    answer += i;
+                }
+                
+            }
+        }
+        return answer;
+    }
+}
