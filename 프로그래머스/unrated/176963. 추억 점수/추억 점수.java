5625cee7fe586574acc6be686f6d1472d6088105
林지현 <103114387+mic050r@users.noreply.github.com>
2023-06-10 01:42:22 +0900

    [unrated] Title: 추억 점수, Time: 0.04 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        int[] answer = new int[photo.length];
+        for(int i = 0; i < name.length; i++){
+            map.put(name[i], yearning[i]);
+        }
+        
+       for(int i = 0; i < photo.length; i++){
+            String[] persons = photo[i]; 
+            int score = 0;
+            for(int j = 0; j < persons.length; j++){
+                String person = persons[j];
+                if(map.containsKey(person)){ 
+                    score += map.get(person); 
+                }
+            }
+            answer[i] = score;
+        }
+        return answer;
+    }
+}
