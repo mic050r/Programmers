cbbe969d1aad976a5de8117a6e241a783378b757
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-13 15:42:51 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,7 +1,3 @@
 function solution(my_string, is_prefix) {
-    var answer = 0;
-    for(let i = 0; i < my_string.length; i++){
-        if(my_string.substring(0,is_prefix.length) === is_prefix) answer = 1;
-    }
-    return answer;
+    return my_string.substring(0,is_prefix.length) === is_prefix ? 1 : 0;
 }
