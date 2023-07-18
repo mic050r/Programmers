df3c08b808fb8f5ef095124576422d2bb10b815a
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-19 02:01:11 +0900

    [level 1] Title: 소수 찾기, Time: 16.46 ms, Memory: 55.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int sieve[] = new int[n+1];
+        int cnt = 0;
+        for(int i = 2; i <= n; i++){
+	        if(sieve[i] == 1) continue;
+	        for (int j = 2*i; j<=n; j+=i){
+		        sieve[j] = 1; 
+	        }
+        }
+        for(int i = 2; i <= n; i++){
+            if(sieve[i] == 0) cnt++;
+        }
+        return cnt;
+    }
+}
