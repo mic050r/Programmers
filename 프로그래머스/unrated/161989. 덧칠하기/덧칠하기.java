d0e37f0bc27f3cb00d9a1177cfc4e3b9e61ee07f
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-22 22:25:50 +0900

    [unrated] Title: 덧칠하기, Time: 1.27 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int secList[] = new int[n+1];
         int cnt = 0;
-        // 1. 다시 칠해야하는 곳 곳 = 1, 칠한 곳 = 0
+        // 1. 다시 칠해야하는 곳 = 1, 칠한 곳 = 0
         for(int sec : section){
             secList[sec]++;
         }
