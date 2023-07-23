d00ef9c594bcf112e0995044fea3ee2050aa3f46
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-24 01:21:17 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,14 +1,3 @@
 function solution(n) {
-    // 변수 선언
-    var list = "";
-    let answer = "";
-    // 내림차순
-    list += (n.toString()).split("").sort((a,b)=> b-a);
-    
-    // 배열을 문자열로
-    for(let i of list){
-        if(i === ',') continue;
-        answer += i;
-    }
-    return Number(answer);
+    return parseInt((n+"").split("").sort().reverse().join(""));
 }
