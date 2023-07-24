02f987d0914adac1582ad0a2b186881bdfe8811f
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-25 01:15:34 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.17 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/README.md
+++ 프로그래머스/unrated/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
 
 ### 성능 요약
 
-메모리: 74.6 MB, 시간: 0.36 ms
+메모리: 82.1 MB, 시간: 0.17 ms
 
 ### 구분
 
--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,15 +1,19 @@
 class Solution {
+
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        for(int i = 1; i <= number; i++){
-            int cnt = 0;
-            for(int j = 1; j*j <= i; j++){
-                if(i%j == 0){
-                    cnt += (j*j == i) ? 1 : 2;
-                }
+        int[] count = new int[number + 1];    
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            if(cnt > limit) cnt = power;
-            answer += cnt;
+        }
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
         }
         return answer;
     }
