59041f75bc565796b733be90dee04aedab149783
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-25 01:08:56 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.70 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,7 +1,6 @@
 function solution(number, limit, power) {
-    let answer = [];
+    let answer = 0;
   
-    // 1. 약수 개수 구하기
     for (let i = 1; i <= number; i++) {
         let cnt = 0;
         for (let j = 1; j * j <= i; j++) {
@@ -9,18 +8,9 @@ function solution(number, limit, power) {
                 cnt += (j * j === i) ? 1 : 2;
             }
         }
-        answer.push(cnt);
+        if(cnt > limit) cnt = power;
+        answer += cnt
     }
   
-    // 2. 제한 수치 구하기
-    for (let i = 0; i < answer.length; i++) {
-        if (answer[i] > limit) {
-            answer[i] = power;
-        }
-    }
-  
-    // 3. 합 구하기
-    const result = answer.reduce((acc, val) => acc + val, 0);
-  
-    return result;
+    return answer;
 }
