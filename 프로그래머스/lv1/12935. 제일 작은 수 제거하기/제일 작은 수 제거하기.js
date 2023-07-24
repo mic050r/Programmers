09edaa3d32cf44ef385d2ebef16618fa36f38810
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-24 17:10:24 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,5 +1,6 @@
 function solution(arr) {
-    // 다른 사람의 풀이1
-    const min = Math.min(...arr);
-    return arr.length !== 1 ? arr.filter(i => i !== min) : [-1]
+    // 다른 사람의 풀이2
+    arr.splice(arr.indexOf(Math.min(...arr)),1);
+    if(arr.length<1)return[-1];
+    return arr;
 }
