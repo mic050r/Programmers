ec3c6b1f4aa2df1ee7010ba14e4de4ad888eb355
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-25 00:20:08 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,14 +1,3 @@
 function solution(numbers) {
-    var answer = 0;
-    let x = 0;
-    numbers.sort((a, b) => a - b);
-    
-    for(let i = 0; i <= 9; i++){
-        if(numbers[x] !== i){
-            answer += i; 
-            x--;
-        }
-        x++;
-    }
-    return answer;
+    return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
 }
