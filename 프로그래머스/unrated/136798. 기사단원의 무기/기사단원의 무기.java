fa79ce137b5d930341d69003d77a3bba8e3a5c75
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-26 19:27:19 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.36 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,19 +1,15 @@
 class Solution {
-
     public int solution(int number, int limit, int power) {
-        int[] count = new int[number + 1];    
-        for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= number / i; j++) {
-                count[i * j]++;
-            }
-        }
         int answer = 0;
-        for (int i = 1; i <= number; i++) {
-            if (count[i] > limit) {
-                answer += power;
-            } else {
-                answer += count[i];
+        for(int i = 1; i <= number; i++){
+            int cnt = 0;
+            for(int j = 1; j*j <= i; j++){
+                if(i%j == 0){
+                    cnt += (j*j == i) ? 1 : 2;
+                }
             }
+            if(cnt > limit) cnt = power;
+            answer += cnt;
         }
         return answer;
     }
