fb07aedf767f3550debf4b902cc53326888a7aaa
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-28 00:14:33 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,6 +1,3 @@
 function solution(my_string) {
-    const answer = my_string.replace(/[^0-9]/g, '')
-                            .split('')
-                            .reduce((acc, curr) => acc + Number(curr), 0);
-    return answer;
+    return [...my_string].reduce((acc,cur)=>Number(cur) ? +acc + +cur : acc, 0)
 }
