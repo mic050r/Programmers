3ae8822398c8c43e29428c5e4d050998d13cc3b9
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-27 23:59:19 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 0.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,7 +1,3 @@
 function solution(myString, pat) {
-    myString = myString.toLowerCase();
-    pat = pat.toLowerCase();
-    // if(myString.length < pat.length) return 0;
-    if(myString.includes(pat)) return 1;
-    else return 0;
+    return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
 }
