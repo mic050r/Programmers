c4b98676d3e4b7206d8853ee0304768b41d5a47b
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-30 03:14:09 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,9 +1,12 @@
+/*
+ 가위는 2 바위는 0 보는 5
+*/
 function solution(rsp) {
-    let result = '';
-    for(let i of rsp){
-        if(i === '2') result += '0'
-        else if(i === '0') result += '5'
-        else if(i === '5') result += '2'
-    }
-    return result;
+    let arr = {
+        2: 0,
+        0: 5,
+        5: 2
+    };
+    var answer = [...rsp].map(v => arr[v]).join("");
+    return answer;
 }
