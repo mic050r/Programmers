043783d1f3f004f1219d15a8665ad9c7564e249f
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-30 00:56:35 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,36 +1,10 @@
 function solution(lottos, win_nums) {
-    var answer = [];
-    // 맞힌 개수 +- 0의 갯수 
-    let correct = 0, zero = 0;
-    let max, min = 0;
-    
-    for (let i of lottos) {
-        if (i === 0) zero++;
-        else answer.push(i);
-    }
+    const rank = [6, 6, 5, 4, 3, 2, 1];
 
-    for (let i of answer) {
-        for (let j of win_nums) {
-            if (i === j) correct++;
-        }
-    }
+    let minCount = lottos.filter(v => win_nums.includes(v)).length;
+    let zeroCount = lottos.filter(v => !v).length;
 
-    max = correct_cnt(correct + zero);
-    min = correct_cnt(correct);
-    
-    return [max, min];
-}
+    const maxCount = minCount + zeroCount;
 
-// 순위 정하기
-const correct_cnt = function(score) {
-    let result = 0;
-    switch (score) {
-        case 6: result = 1; break;
-        case 5: result = 2; break;
-        case 4: result = 3; break;
-        case 3: result = 4; break;
-        case 2: result = 5; break;
-        default: result = 6; break;
-    }
-    return result;
+    return [rank[maxCount], rank[minCount]];
 }
