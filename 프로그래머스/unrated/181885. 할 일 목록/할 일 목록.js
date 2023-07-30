3849819f0dd210adf555892fd34c49453c315a4e
林지현 <103114387+mic050r@users.noreply.github.com>
2023-07-30 20:40:30 +0900

    [unrated] Title: 할 일 목록, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -1,7 +1,3 @@
 function solution(todo_list, finished) {
-    var answer = [];
-    for(let i = 0; i < todo_list.length; i++){
-        if(finished[i] === false) answer.push(todo_list[i]);
-    }
-    return answer;
+    return todo_list.filter((e,i) => !finished[i]);
 }
