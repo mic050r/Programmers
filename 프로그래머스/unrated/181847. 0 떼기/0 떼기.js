c9ec235fedc56687f370bf2a46b7b682bc42a5d3
林지현 <103114387+mic050r@users.noreply.github.com>
2023-08-01 22:18:05 +0900

    [unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,15 +1,1 @@
-function solution(n_str) {
-    let answer = '';
-    let len = n_str.length;
-    let foundNonZero = false;
-
-    for (let i = 0; i < len; i++) {
-        if (!foundNonZero && n_str[i] !== '0') {
-            foundNonZero = true;
-        }
-        if (foundNonZero) {
-            answer += n_str[i];
-        }
-    }
-    return answer;
-}
+const solution = (str) => String(Number(str))
