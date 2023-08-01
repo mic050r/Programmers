4869ce80a416e0da8ea62e5cb99e5219af9b564f
林지현 <103114387+mic050r@users.noreply.github.com>
2023-08-01 22:14:53 +0900

    [unrated] Title: 0 떼기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,12 +1,14 @@
 function solution(n_str) {
-    var answer = '';
+    let answer = '';
     let len = n_str.length;
-    for(let i = 0; i < len; i++){
-        if(n_str[i] !== '0'){
-            for(let j = i; j < len; j++){
-                answer += n_str[j];
-            }
-            break;
+    let foundNonZero = false;
+
+    for (let i = 0; i < len; i++) {
+        if (!foundNonZero && n_str[i] !== '0') {
+            foundNonZero = true;
+        }
+        if (foundNonZero) {
+            answer += n_str[i];
         }
     }
     return answer;
