5d3aa3b3433d27e3444d5e2b8d2466a72684f4c3
林지현 <103114387+mic050r@users.noreply.github.com>
2023-08-04 13:16:29 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.89 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,26 +1,14 @@
-import java.util.*;
-
 class Solution {
-    public String solution(String s) {
+  public String solution(String s) {
         String answer = "";
-    	String[] arr = s.split(" ");
-    	for(int i=0; i<arr.length; i++) {
-    		String str = arr[i];
- 
-    		if(arr[i].length() == 0) {
-    			answer += " ";
-    		} 
-    		else {
-    			answer += str.substring(0, 1).toUpperCase();
-    			answer += str.substring(1, str.length()).toLowerCase();
-    			answer += " ";
-    		}
-    		
-    	}
-    	if(s.substring(s.length()-1, s.length()).equals(" ")){
-    		return answer;
-    	}
-        return answer.substring(0, answer.length()-1);
-    }
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
 
+        for(String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
+        }
+
+        return answer;
+  }
 }
