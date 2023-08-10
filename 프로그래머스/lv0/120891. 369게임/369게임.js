5c74552fdd9369365e2424ecc642862a07b48f2c
林지현 <103114387+mic050r@users.noreply.github.com>
2023-08-11 00:26:07 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -0,0 +1,10 @@
+function solution(order) {
+    let answer = 0;
+    let str = order.toString();
+    for(let i of str){
+        if(i == 3 || i == 6 || i == 9){
+            answer++;
+        }
+    }
+    return answer;
+}
