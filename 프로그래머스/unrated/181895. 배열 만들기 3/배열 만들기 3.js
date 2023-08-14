d004452e98c0a696892bd3976b8eb773aad65195
林지현 <103114387+mic050r@users.noreply.github.com>
2023-08-15 01:36:44 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.72 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,1 +1,5 @@
-const solution = (arr,intervals) => arr.slice(intervals[0][0],intervals[0][1] + 1).concat(arr.slice(intervals[1][0],intervals[1][1] + 1));
+function solution(arr, intervals) {
+    const [[a,b],[c,d]] = intervals;
+
+    return [...arr.slice(a, b+1), ...arr.slice(c, d+1)];
+}
