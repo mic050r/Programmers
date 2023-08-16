8704600ff32776d2ba1d394b73ef55c015f88ab7
林지현 <103114387+mic050r@users.noreply.github.com>
2023-08-16 15:58:39 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,12 +1,5 @@
 function solution(my_string) {
     var answer = '';
-    for(let i of my_string){
-        let n = i.charCodeAt();
-        if(97 <= n && n <= 122){
-            answer += i.toUpperCase();
-        }else if(65 <= n && n <= 90){
-            answer += i.toLowerCase();
-        }
-    }
+    for (let c of my_string) answer += c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase();
     return answer;
 }
