3779033941400073cdf0d3893f121619ae3ecbcc
林지현 <103114387+mic050r@users.noreply.github.com>
2023-08-23 10:09:48 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,3 +1,8 @@
-const solution = (n) => {
-    return parseInt([...n.toString(3)].reverse().join(""), 3);
+function solution(n) {
+    const answer = [];
+    while(n !== 0) {
+        answer.unshift(n % 3);
+        n = Math.floor(n/3);
+    }
+    return answer.reduce((acc,v,i) => acc + (v * Math.pow(3, i)),0);   
 }
