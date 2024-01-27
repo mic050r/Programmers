ca6f254cb1e82c04c9fc120e95ffe71d318ea9a7
林지현 <103114387+mic050r@users.noreply.github.com>
2024-01-28 00:54:12 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        while(n != 1) {
+            answer.add(n);
+            if(n % 2 == 0) n = n/2;
+            else if(n % 2 != 0) n = 3 * n + 1;
+        }
+        answer.add(1);
+        return answer;
+    }
+}
