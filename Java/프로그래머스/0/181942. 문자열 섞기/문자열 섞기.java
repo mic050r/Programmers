32d8930e5f533c9272b0aa7f10860fd2232f80ee
林지현 <103114387+mic050r@users.noreply.github.com>
2024-03-15 23:21:11 +0900

    [level 0] Title: 문자열 섞기, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Solution {
     public StringBuilder solution(String str1, String str2) {
         StringBuilder answer = new StringBuilder();
-        String len = str1.concat(str2);
+        
         for(int i = 0; i < str1.length(); i++){
             answer.append(str1.charAt(i));
             answer.append(str2.charAt(i));
