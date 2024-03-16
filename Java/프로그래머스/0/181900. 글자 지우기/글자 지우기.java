16a49a7fe43e2f2fe88fbc6e7b56be0021b5f634
林지현 <103114387+mic050r@users.noreply.github.com>
2024-03-17 01:33:13 +0900

    [level 0] Title: 글자 지우기, Time: 0.39 ms, Memory: 75.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,10 +1,7 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
-        StringBuilder sb = new StringBuilder(my_string);
-        for(int i = 0; i < indices.length; i++) {
-            sb.setCharAt(indices[i] , '7');
-        }
-        String result = sb.toString().replaceAll("7", "");
-        return result;
+        String[] str = my_string.split("");
+        for (int i = 0;i < indices.length; i++) str[indices[i]] = "";
+        return String.join("",str);
     }
 }
