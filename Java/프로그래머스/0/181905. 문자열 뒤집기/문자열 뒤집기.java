b0a99d169806e82faca52a3a1ab91482affafe39
林지현 <103114387+mic050r@users.noreply.github.com>
2024-03-17 16:49:22 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder answer = new StringBuilder();
+        StringBuilder str = new StringBuilder(my_string);
+        
+        if (s == e) {
+            return my_string;
+        }
+        
+        answer.append(str.substring(0, s)) 
+              .append((new StringBuilder(str.substring(s, e + 1)).reverse()))
+              .append(str.substring(e + 1));
+              
+        return answer.toString();
+    }
+}
