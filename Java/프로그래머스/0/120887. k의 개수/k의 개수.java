7f4ed4002ceadf7ce6191dddd067a5b2cce76f9d
林지현 <103114387+mic050r@users.noreply.github.com>
2024-03-20 09:48:06 +0900

    [level 0] Title: k의 개수, Time: 15.76 ms, Memory: 80.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120887. k의 개수/k의 개수.java
+++ Java/프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int num1, int num2, int k) {
         int answer = 0;
-        String code = Integer.toString(k);
-        char targetChar = code.charAt(0);
+        
+        char targetChar = (k + "").charAt(0);
+        
         for(int i = num1; i <= num2; i++){
-            String str = Integer.toString(i);
+            String str = i+"";
             for(int j = 0; j < str.length(); j++) {
                 if(str.charAt(j) == targetChar) answer++; 
             }
