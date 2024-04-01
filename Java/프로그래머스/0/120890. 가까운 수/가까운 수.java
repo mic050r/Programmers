df646de976d8136c06d371c6e26595f3cff7316f
林지현 <103114387+mic050r@users.noreply.github.com>
2024-04-02 08:52:50 +0900

    [level 0] Title: 가까운 수, Time: 0.45 ms, Memory: 77.6 MB -BaekjoonHub

--- Java/프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ Java/프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -1,18 +1,15 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] array, int n) {
         int answer = 0;
-        int min = 101; // 최대값으로 초기화
-        
-        for(int i = 0; i < array.length; i++){
-            int cha = Math.abs(n - array[i]); // 현재 값과 n의 차이의 절대값 계산
-            if(cha < min) {
-                min = cha; // 최솟값 업데이트
-                answer = array[i]; // 가장 가까운 수 업데이트
-            } else if (cha == min) {
-                answer = Math.min(answer, array[i]); // 최솟값이 같은 경우 더 작은 수로 업데이트
+        Arrays.sort(array);
+        for(int i = 1 ; i < array.length ; i++){
+            if(Math.abs(n-array[0]) > Math.abs(n-array[i])){
+                array[0] = array[i];
             }
         }
-        
+        answer = array[0];
         return answer;
     }
 }
