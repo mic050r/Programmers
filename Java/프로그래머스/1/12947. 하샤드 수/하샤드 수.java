3713697b68875550eaad7b06050d1d33c73b1501
林지현 <103114387+mic050r@users.noreply.github.com>
2024-04-26 09:26:41 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int n = x;
+        // 1. 자릿수의 합구하기
+        while(n != 0) {
+            sum += n%10;
+            n /= 10;
+        }
+        
+        // 2. 자릿수의 합이 x와 나누어 떨어지는지
+        return (x % sum == 0) ? true : false;
+    }
+}
